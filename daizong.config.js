ae959b331f43c63f70c6e8a0499757b6d19b48ac
Mgen <m@mgenware.com>
2021-12-12 14:53:27 +0800

    Update daizong.config.js

--- daizong.config.js
+++ daizong.config.js
@@ -5,27 +5,27 @@ export default {
     envGroups: ['development'],
   },
 
-  // Runs tests (you need to build the project first).
-  t: {
-    run: 'mocha --parallel --require source-map-support/register dist_tests/**/*.test.js',
-  },
-
   // Cleans, lints, compiles sources and runs tests.
   build: {
     run: ['#clean', 'tsc -b tests', '#lint', '#t'],
     envGroups: ['production'],
   },
 
-  // Deletes compiled files, auto triggered by `yarn r dev` or `yarn r build`.
+  // Deletes compiled files, auto triggered by `#dev` or `#build`.
   clean: {
     run: {
       del: ['dist', 'dist_tests'],
     },
   },
 
-  // Lints the project using ESLint, auto triggered by `yarn r build`.
+  // Lints the project using ESLint, auto triggered by `#build`.
   lint: {
     run: 'eslint --max-warnings 0 --ext .ts src/ tests/',
+  },
+
+  // Runs tests when you already have `#dev` running.
+  t: {
+    run: 'mocha --parallel --require source-map-support/register dist_tests/**/*.test.js',
   },
 
   _: {
